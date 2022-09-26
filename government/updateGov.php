123dec5cf98c463f64bd02e8dca3e1c37e8ac798
Engsondos <engsondosahmed1@gmail.com>
2022-09-26 15:07:52 +0200

    government

---
+++ government/updateGov.php
@@ -0,0 +1,68 @@
+<?php 
+$title = "Update Government";
+include "../general/env.php";
+include "../layout/header.php";
+$id = "";
+
+if(isset($_GET['edit'])){
+    $id = $_GET['edit'];
+    $select =  "SELECT * FROM `government` WHERE id = $id";
+    $query = mysqli_query($connect,$select);
+    if($query){
+        $row = mysqli_fetch_assoc($query);
+    }else{
+        $error ="<div class = 'alert alert-danger'> Some Thing Wrong</div>";
+    }
+}
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $update =  "UPDATE `government` SET `name` = '$name' WHERE id =$id";
+    // echo $update;
+    $query = mysqli_query($connect,$update);
+    if($query){
+        $_SESSION['editsuc']="<div class = 'alert alert-success'> Updated Successfully</div>";
+        header("location:ShowGov.php");
+
+    }else{
+        $error ="<div class = 'alert alert-danger'> Some Thing Wrong</div>";
+    }
+}
+if(isset($_GET['delete'])){
+    $id=$_GET['delete'];
+    $delete =  "DELETE From `government` WHERE id =$id";
+    // echo $update;
+    $query = mysqli_query($connect,$delete);
+    if($query){
+        $_SESSION['deletesuc']="<div class = 'alert alert-success'> Deleted Successfully</div>";
+        header("location:ShowGov.php");
+
+    }else{
+        $error ="<div class = 'alert alert-danger'> Some Thing Wrong</div>";
+    }
+}
+?>
+<div class="container">
+    <div class="row">
+        <div class="offset-3 col-6 mt-5 text-center h1 text-primary">
+            <?= $title?>
+        </div>
+        <div class="offset-4 col-4 mt-5">
+            <form method="post">
+                <div class="mb-3">
+                    <input type="text" placeholder="Name" class="form-control" name="name" value="<?=$row['name']?>">
+
+                </div>
+                <?= $error ?? "" ?>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary form-control" name="submit">Update</button>
+
+
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+<?php include "../layout/footer.php";
+?>
